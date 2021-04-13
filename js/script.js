f81a0be5613712396c7c12b90c54c5d33997bc9d
IvannaHavryliukFEt <gavryliuk.ivanna@gmail.com>
2021-04-13 21:54:00 +0300

    started working on modal

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// case 1
+const btnContact = document.getElementById("btnContact");
+const modal = document.getElementsByClassName("modal")[0];
+btnContact.addEventListener("click", () => {
+  modal.classList.add("visible");
+});
+
+// case 2
+const closeModal = () => {
+  modal.classList.remove("visible");
+};
